9707c7ebe62b1f3e6ef9131c2b9ea997ce83eb93
IlyassLho <ilyasslhouari@gmail.com>
2025-11-27 21:10:55 +0100

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './pages/Home/Home';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/detail/:imdb_id" element={<MovieDetailPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
