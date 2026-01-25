6002ab9a00b38ef7e4d3a7f9c499db3031ed62a5
IlyassLho <ilyasslhouari@gmail.com>
2026-01-25 17:30:25 +0100

    Enhance: Load watchlist from local storage and persist changes to local storage on updates

--- src/redux/watchlistSlice.js
+++ src/redux/watchlistSlice.js
@@ -1,7 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// load the watchlist from the local storage if it exists
+const loadFromStorage = () => {
+  const savedData = localStorage.getItem("myWatchlist");
+  return savedData ? JSON.parse(savedData) : [];
+};
+
 const initialState = {
-  movies: [],
+  movies: loadFromStorage(),
 };
 
 export const watchlistSlice = createSlice({
@@ -12,10 +18,12 @@ export const watchlistSlice = createSlice({
       const exists = state.movies.find((movie) => movie.id === action.payload.id);
       if (!exists) {
         state.movies.push(action.payload);
+        localStorage.setItem("myWatchlist", JSON.stringify(state.movies));
       }
     },
     removeFromWatchlist: (state, action) => {
       state.movies = state.movies.filter((movie) => movie.id !== action.payload.id);
+      localStorage.setItem("myWatchlist", JSON.stringify(state.movies));
     },
   },
 });
